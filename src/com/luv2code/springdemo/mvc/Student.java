134a5b5d7d7b6c610c3a26b44364c6d3f129b004
Hanna Lembak <lembakhanna@gmail.com>
2023-02-09 16:06:53 +0200

    Adding MAp of country

--- src/com/luv2code/springdemo/mvc/Student.java
+++ src/com/luv2code/springdemo/mvc/Student.java
@@ -1,21 +1,32 @@
 package com.luv2code.springdemo.mvc;
 
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+
 public class Student {
     private String firstName;
 
     private String lastName;
 
+    private LinkedHashMap<String,String> countryOption;
     private String country;
 
     public String getCountry() {
         return country;
     }
 
+
     public void setCountry(String country) {
         this.country = country;
     }
 
     public Student() {
+        // populate county options: use ISO country code
+        countryOption  = new LinkedHashMap<>();
+        countryOption.put("BR","Brazil");
+        countryOption.put("UA","Ukraine");
+        countryOption.put("PL","Poland");
+        countryOption.put("RM", "Romania");
     }
 
     public String getFirstName() {
@@ -32,5 +43,9 @@ public class Student {
 
     public void setLastName(String lastName) {
         this.lastName = lastName;
+    }
+
+    public LinkedHashMap<String, String> getCountryOption() {
+        return countryOption;
     }
 }
